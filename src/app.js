242488bdeb932a909bfef67ae5149818d0ef38af
Clara Espinosa Pantoja <claritapantoja36@gmail.com>
2025-01-14 23:56:00 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,21 +14,7 @@ window.onload = function() {
 };
 
 function generateCard() {
-  let cardNumber = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
+  let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   let suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
   let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
   let randomSuitNumber = Math.floor(Math.random() * suit.length);
